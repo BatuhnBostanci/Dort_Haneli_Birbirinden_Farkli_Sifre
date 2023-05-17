7d39eac87efdcf1fe20c2dde7d1edb0a791303b9
bbostanci <bbostanci@BBOSTANCI>
2023-05-17 11:12:47 +0300

    Proje dosyası ekle.

---
+++ DortHaneli150TaneSifreUret/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var passwordList = new List<string>();
+var random = new Random();
+const string characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+while (passwordList.Count() < 150)
+{
+    var password = new string(Enumerable.Repeat(characters, 4)
+        .Select(s => s[random.Next(s.Length)]).ToArray());
+
+    if (!passwordList.Contains(password))
+    {
+        passwordList.Add(password);
+    }
+}
+
+foreach (var password in passwordList)
+{
+    Console.WriteLine(password);
+}
+Console.ReadLine();
